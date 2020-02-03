529066e5ca7c5a1db3da841ac86dad170398bed9
ngjamesng <just.james.ng@gmail.com>
2020-02-03 11:10:49 -0800

    created cupcake model

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, render_template, redirect
+from models import db,  connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
--- models.py
+++ models.py
@@ -1,1 +1,19 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+
+    __tablename__= "cupcakes"
+
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor=db.Column(db.String(30), nullable=False)
+    size=db.Column(db.String(30), nullable=False)
+    rating=db.Column(db.Float, nullable=False)
+    image=db.Column(db.Text, default="https://thestayathomechef.com/wp-content/uploads/2017/12/Most-Amazing-Chocolate-Cupcakes-1-small.jpg", nullable=False)
+
